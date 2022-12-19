4f7b67defa746cf7359db2ab65d1ec4ec080bfcd
Nick Dichev <github@dichev.email>
2022-12-19 13:18:19 -0800

    Move Elixir config to lsp

--- nvim/.config/nvim/lua/lsp.lua
+++ nvim/.config/nvim/lua/lsp.lua
@@ -1,26 +1,71 @@
-local nvim_lsp = require('lspconfig')
+local opts = { noremap=true, silent=true }
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, opts)
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, opts)
 
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
-
   -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
 
   -- Mappings.
-  local opts = { noremap=true, silent=true }
-
   -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>', opts)
-  buf_set_keymap('n', '[e', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']e', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<leader>mf', '<cmd>lua vim.lsp.buf.format({ async = true })<CR>', opts)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+  vim.keymap.set('n', '<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workleader_folders()))
+  end, bufopts)
+  vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
 end
+
+local on_attach_elixir = function(client, bufnr)
+  local map_opts = { buffer = true, noremap = true}
+
+  -- run the codelens under the cursor
+  vim.keymap.set("n", "<leader>r",  vim.lsp.codelens.run, map_opts)
+  -- remove the pipe operator
+  vim.keymap.set("n", "<leader>fp", ":ElixirFromPipe<cr>", map_opts)
+  -- add the pipe operator
+  vim.keymap.set("n", "<leader>tp", ":ElixirToPipe<cr>", map_opts)
+  vim.keymap.set("v", "<leader>em", ":ElixirExpandMacro<cr>", map_opts)
+
+  -- keybinds for vim-vsnip: https://github.com/hrsh7th/vim-vsnip
+  vim.cmd([[imap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']])
+  vim.cmd([[smap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']])
+
+  return on_attach(client, bufnr)
+end
+
+local lsp_flags = {
+  -- This is the default in Nvim 0.7+
+  debounce_text_changes = 150,
+}
+
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+require('lspconfig')['elixirls'].setup{
+  cmd = { os.getenv('HOME') .. '/.ls/elixir-ls/language_server.sh' },
+  on_attach = on_attach_elixir,
+  capabilities = capabilities,
+  settings = {
+    dialyzerEnabled = true,
+    fetchDeps = false,
+    enableTestLenses = false,
+    suggestSpecs = false,
+  }
+}
 
 require('nvim-treesitter.configs').setup({
   ensure_installed = {
--- nvim/.config/nvim/lua/plugin/elixir.lua
+++ nvim/.config/nvim/lua/plugin/elixir.lua
@@ -1,31 +1,5 @@
 local elixir = require('elixir')
 
 elixir.setup({
-  tag = 'v0.12.0',
-
-  settings = elixir.settings({
-    dialyzerEnabled = true,
-    fetchDeps = false,
-    enableTestLenses = false,
-    suggestSpecs = false,
-  }),
-
-  
-  capabilities = require('cmp_nvim_lsp').default_capabilities(),
-
-  on_attach = function(client, bufnr)
-    local map_opts = { buffer = true, noremap = true}
-
-    -- run the codelens under the cursor
-    vim.keymap.set("n", "<leader>r",  vim.lsp.codelens.run, map_opts)
-    -- remove the pipe operator
-    vim.keymap.set("n", "<leader>fp", ":ElixirFromPipe<cr>", map_opts)
-    -- add the pipe operator
-    vim.keymap.set("n", "<leader>tp", ":ElixirToPipe<cr>", map_opts)
-    vim.keymap.set("v", "<leader>em", ":ElixirExpandMacro<cr>", map_opts)
-
-    -- keybinds for vim-vsnip: https://github.com/hrsh7th/vim-vsnip
-    vim.cmd([[imap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']])
-    vim.cmd([[smap <expr> <C-l> vsnip#available(1) ? '<Plug>(vsnip-expand-or-jump)' : '<C-l>']])
-  end
+  cmd = os.getenv('HOME') .. '/.ls/elixir-ls/language_server.sh'
 })
