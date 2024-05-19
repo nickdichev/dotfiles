a059ffd3fb2d41f45529a114c28710ac60022c6e
Nick Dichev <github@dichev.email>
2024-05-19 13:07:14 -0700

    Migrate plugins to their own module

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,17 +1,32 @@
+-- Setup lazy
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
 -- Leader
 -- Must be done before lazy is loaded
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ','
 
-require('plugins')
+-- Point lazy at our plugins module
+require('lazy').setup('plugins')
+
 require('lsp')
 require('options')
 require('completion')
 require('keybinds')
 
-require('plugin/gitsigns')
-require('plugin/lazygit')
-require('plugin/onedark')
-require('plugin/lualine')
-
-require('plugin/elixir')
+-- require('plugin/gitsigns')
+-- require('plugin/lazygit')
+-- require('plugin/onedark')
+-- require('plugin/lualine')
+-- require('plugin/elixir')
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -1,17 +1,4 @@
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
-return require('lazy').setup({
+return {
   'lewis6991/gitsigns.nvim',
   'kdheepak/lazygit.nvim',
   'tpope/vim-eunuch', -- sugar for unix shell commands
@@ -21,8 +8,16 @@ return require('lazy').setup({
   'mhinz/vim-sayonara',
   {'christoomey/vim-tmux-navigator', lazy = false },
 
-  'navarasu/onedark.nvim',
-  'nvim-lualine/lualine.nvim',
+  {'maxmx03/dracula.nvim', config = function ()
+    vim.cmd.colorscheme 'dracula'
+  end},
+
+  {'nvim-lualine/lualine.nvim', opts = {
+    options = {
+      theme = vim.g.colors_name,
+      refresh = {statusline = 1000}
+    }
+  }},
 
   'neovim/nvim-lspconfig',
   'nvim-treesitter/nvim-treesitter-context',
@@ -39,4 +34,4 @@ return require('lazy').setup({
 
   { 'nvim-telescope/telescope.nvim', dependencies = { {'nvim-lua/plenary.nvim'} } },
   { 'elixir-tools/elixir-tools.nvim', dependencies = { 'nvim-lua/plenary.nvim' } },
-})
+}
