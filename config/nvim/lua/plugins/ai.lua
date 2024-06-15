344c5d2e0694d7d3c3d91007284226770f86ec80
Nick Dichev <nick.dichev@fireworkhq.com>
2024-06-15 12:29:04 -0700

    Configure keymaps for CopilotChat

--- config/nvim/lua/plugins/ai.lua
+++ config/nvim/lua/plugins/ai.lua
@@ -1,3 +1,5 @@
+local map = require('core.utils').map
+
 return {
   {
     "zbirenbaum/copilot.lua",
@@ -25,8 +27,18 @@ return {
       { "nvim-lua/plenary.nvim" },
     },
     opts = {
-      -- See Configuration section for rest
     },
-    -- See Commands section for default commands if you want to lazy load on them
+    init = function()
+      local function pick_with_selection(selection)
+        return function()
+          local actions = require("CopilotChat.actions")
+          actions.pick(actions.prompt_actions { selection = require("CopilotChat.select")[selection] })
+        end
+      end
+      map({ "n", "v", "x" }, "<leader>cc", function() require("CopilotChat").toggle() end)
+      map({ "n", "v", "x" }, "<leader>cb", pick_with_selection("buffer"))
+      map({ "n", "v", "x" }, "<leader>ca", pick_with_selection("buffers"))
+      map({ "n", "v", "x" }, "<leader>cs", pick_with_selection("visual"))
+    end,
   },
 }
