358f04ecec1937df482d7993166389ce5319f6bb
Nick Dichev <github@dichev.email>
2025-04-11 16:06:32 -0700

    Remove unecessary comments

--- home.nix
+++ home.nix
@@ -11,13 +11,6 @@
     ./git.nix
   ];
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not change this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
   home.stateVersion = "22.11"; # Please read the comment before changing.
 
   home.packages = [
@@ -52,17 +45,6 @@
   ];
 
   home.file = {
-    # # Building this configuration will create a copy of 'dotfiles/screenrc' in
-    # # the Nix store. Activating the configuration will then make '~/.screenrc' a
-    # # symlink to the Nix store copy.
-    # ".screenrc".source = dotfiles/screenrc;
-
-    # # You can also set the file content immediately.
-    # ".gradle/gradle.properties".text = ''
-    #   org.gradle.console=verbose
-    #   org.gradle.daemon.idletimeout=3600000
-    # '';
-
     ".psql" = {
       source = ./config/psql;
     };
@@ -73,7 +55,6 @@
     ERL_AFLAGS = "-kernel shell_history enabled";
   };
 
-  # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
   programs.bat = {
