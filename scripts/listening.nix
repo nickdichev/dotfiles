a103005a603849a99e1baba94c02d8bf551b76c7
Nick Dichev <github@dichev.email>
2025-08-05 17:08:52 -0700

    Add a -p flag to listening script which outputs the pid

--- scripts/listening.nix
+++ scripts/listening.nix
@@ -11,13 +11,45 @@ writeShellApplication {
   ];
 
   text = ''
-    if [[ $# -eq 0 ]]; then
-      lsof -iTCP -sTCP:LISTEN -n -P
-    elif [[ $# -eq 1 ]]; then
-      lsof -iTCP -sTCP:LISTEN -n -P | rg -i "$1"
+    pid_only=false
+    port=""
+
+    while [[ $# -gt 0 ]]; do
+      case $1 in
+        -p|--pid)
+          pid_only=true
+          shift
+          ;;
+        -h|--help)
+          echo "Usage: listening [-p|--pid] [port]"
+          echo "  -p, --pid    Show only PIDs"
+          echo "  port         Filter by port number"
+          exit 0
+          ;;
+        *)
+          if [[ -z "$port" ]]; then
+            port="$1"
+          else
+            echo "Usage: listening [-p|--pid] [port]"
+            exit 1
+          fi
+          shift
+          ;;
+      esac
+    done
+
+    if [[ -z "$port" ]]; then
+      if [[ "$pid_only" == true ]]; then
+        lsof -iTCP -sTCP:LISTEN -n -P | awk 'NR > 1 {print $2}' | sort -u
+      else
+        lsof -iTCP -sTCP:LISTEN -n -P
+      fi
     else
-      echo "Usage: listening [port]"
-      exit 1
+      if [[ "$pid_only" == true ]]; then
+        lsof -iTCP -sTCP:LISTEN -n -P | rg -i "$port" | awk '{print $2}' | sort -u
+      else
+        lsof -iTCP -sTCP:LISTEN -n -P | rg -i "$port"
+      fi
     fi
   '';
 }
