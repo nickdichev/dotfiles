c6f09b043caa29479410e9d73654fd335a416306
Nick Dichev <github@dichev.email>
2025-10-10 12:25:28 -0700

    Add terraform filetype for tofu-ls

--- config/nvim/lsp/tofu-ls.lua
+++ config/nvim/lsp/tofu-ls.lua
@@ -1,5 +1,5 @@
 return {
   cmd = { "tofu-ls", "serve" },
-  filetypes = { "opentofu", "opentofu-vars" },
+  filetypes = { "opentofu", "opentofu-vars", "terraform", "terraform-vars" },
   root_markers = { ".terraform", ".git" },
 }
