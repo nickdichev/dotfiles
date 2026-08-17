1abbc80287dff5e1051f126f0e986519dedff888
Nick Dichev <git@dichev.email>
2026-08-17 16:43:30 -0700

    Update Hermes Desktop to 0.18.2

--- pkgs/hermes-desktop/default.nix
+++ pkgs/hermes-desktop/default.nix
@@ -6,8 +6,8 @@
 }:
 
 let
-  version = "0.17.0";
-  rev = "413ed6b9df18f22152d26b6de4093280dcb2b16b";
+  version = "0.18.2";
+  rev = "9de9c25f620ff7f1ce0fd5457d596052d5159596";
 in
 buildNpmPackage {
   pname = "hermes-desktop";
@@ -17,11 +17,11 @@ buildNpmPackage {
     owner = "NousResearch";
     repo = "hermes-agent";
     inherit rev;
-    hash = "sha256-wek7P9tdrqRk/ArxBXt/X3BJpQ+wcnKFqG0S+ryx5p0=";
+    hash = "sha256-RieWkLWEn21aamFvQTTnJwwQl00JJGgp3LvY3D3G3jQ=";
   };
 
   npmWorkspace = "apps/desktop";
-  npmDepsHash = "sha256-RqQyEqzeSlywA83+S9d0KXBnirIVPNFixXCyaE523zo=";
+  npmDepsHash = "sha256-qDXGL/INHPW0pTF4SRVL1dS5XVh2X85dEE4JhrAQeqU=";
 
   env = {
     ELECTRON_SKIP_BINARY_DOWNLOAD = "1";
